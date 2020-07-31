9b09c028dee68ac488004473024e659ae16d7166
Chubbray <chubbray1@gmail.com>
2020-07-31 15:33:49 -0600

    added to seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,13 +2,28 @@ USE employeeTracker_db;
 
 INSERT INTO department (name)
 VALUES
-("housing");
+("Sales"),
+("Engineering"),
+("Finance"),
+("Legal");
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-("leasing agent", 50000.00, 1),
-(" ");
+("Sales Lead", 100000.00, "Sales"),
+("Salesperson", 80000.00, "Sales"),
+("Lead Engineer", 150000.00, "Engineering"),
+("Software Engineer", 120000.00, "Engineering"),
+("Accountant", 125000.00, "Finace"),
+("Legal Team Lead", 250000.00, "Legal"),
+("Lawyer", 190000.00, "Legal");
 
 INSERT INTO employee (first_name, last_name, role_id, maneger_id)
 VALUES
-("John", "Doe", 1, 3);
+("John", "Doe", 1, 3),
+("Mike", "Chan", 2, 1),
+("Ashley", "Rodriguez", 3, null),
+("Kevin", "Tupik", 4, 3),
+("Malia", "Brown", 6, null),
+("Sarah", "Lourd", 7, null),
+("Tom", "Galal", 8, 7),
+("Christian", "Eckenrode", 9, 2);
